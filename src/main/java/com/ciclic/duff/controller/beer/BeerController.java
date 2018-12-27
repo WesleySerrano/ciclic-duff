961fdc62db6c82a921e08c9a86d016d01921a426
Wesley Serrano <wesleyserrano@gmail.com>
2018-12-26 23:24:26 -0200

    - Receiving temperature from HTTP POST;

--- src/main/java/com/ciclic/duff/controller/beer/BeerController.java
+++ src/main/java/com/ciclic/duff/controller/beer/BeerController.java
@@ -1,5 +1,8 @@
 package com.ciclic.duff.controller.beer;
 
+import com.ciclic.duff.dto.TemperatureDTO;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,11 @@ public class BeerController
     public String helloBeer()
     {
         return "Hello! Let's have some beer?";
+    }
+
+    @RequestMapping(value="/beerAndMusic",method=RequestMethod.POST)
+    public String getBeerAndMatchingPlaylist(@RequestBody TemperatureDTO temperature)
+    {
+        return ""+temperature.getTemperature();
     }
 }
