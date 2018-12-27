84f716e199676f1bf00aef8e6ea1a06dc1c159c8
Wesley Serrano <wesleyserrano@gmail.com>
2018-12-26 23:32:36 -0200

    - Adding DTO for playlist;

--- src/main/java/com/ciclic/duff/controller/beer/BeerController.java
+++ src/main/java/com/ciclic/duff/controller/beer/BeerController.java
@@ -10,12 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BeerController
 {
+    /**
+     * URL for testing the application
+     */
     @RequestMapping(value="/",method=RequestMethod.GET)
     public String helloBeer()
     {
         return "Hello! Let's have some beer?";
     }
 
+    /**
+     * Gets a temperature value and returns a beer style with a list of playlists matching it
+     * 
+     * @param temperature the temperature value to look for a matching beer style
+     */
     @RequestMapping(value="/beerAndMusic",method=RequestMethod.POST)
     public String getBeerAndMatchingPlaylist(@RequestBody TemperatureDTO temperature)
     {
